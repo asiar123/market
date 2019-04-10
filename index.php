45888729614088e892436578079a43efb5ad1a02
Msbh <michael_2504@hotmail.com>
2019-04-10 13:14:23 -0500

    10/04/2019

---
+++ index.php
@@ -0,0 +1,79 @@
+<html>
+
+		<head>
+		
+		</head>
+		
+		
+		
+		<body bgcolor="#DCDCDC">
+		
+		<form name="form1" method="post" action="insert_prod.php">
+		
+		<table border="0" align="center" width=30%>
+		
+		
+		<tr>
+			<td></td>
+			<td></td>
+			<td align="center">BIENVENIDO</td><br/>
+			<td></td>
+			<td></td>
+			
+		<tr>
+		
+		
+		<tr>
+			<td></td>
+			<td align="center">Codigo Producto:</font></td>
+			<td></td>
+			<td align="center"><input type="text" name="codprod"></td>
+			<td></td>
+		<tr>
+		
+		<tr>
+			<td></td>
+			<td align="center">Nombre Producto:</td>
+			<td></td>
+			<td align="center"><input type="text" name="nomprod"></td>
+			<td></td>
+		<tr>
+		
+		<tr>
+			<td></td>
+			<td align="censter">Cantidad:</td>
+			<td></td>
+			<td align="center"><input type="number" name="cantprod"></td>
+			</select>
+			<td></td>
+		<tr>
+		
+		<tr>
+			<td></td>
+			<td align="center">Estado:*</td>
+			<td></td>
+
+			<td align="center">
+					<select name="estprod">
+							<option value="1"> </option>
+							<option value="0">desabilitado</option>
+							<option value="1">habilitado</option>
+					</select>
+
+			</td> 
+
+			<td></td>
+		<tr>
+		
+		<tr>
+		
+			<td align="center"> <input type="submit" value="Registrar"> </td><br/>
+			
+			
+	    <tr>
+		
+
+		</body>
+		
+		
+</html>
